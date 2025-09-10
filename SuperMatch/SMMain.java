24c0ff0bbb80edd5efd989564f5ccfea342ac50f
Michael <MichaelWidler@yahoo.com>
2025-09-10 13:26:41 -0700

    Initial commit.

---
+++ SuperMatch/SMMain.java
@@ -0,0 +1,19 @@
+package SuperMatch;
+
+import javax.swing.*;
+
+public class SMMain extends JFrame
+{
+   public SMMain()
+   {
+      super();
+      setSize(800, 800);
+      setVisible(true);
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+   }
+   
+   public static void main(String[] args)
+   {
+      SMMain main = new SMMain();
+   }
+}
