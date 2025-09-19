f23489c6b238f74598741aa40c71cd2408e2cd1a
Michael <MichaelWidler@yahoo.com>
2025-09-19 10:27:26 -0700

    Calculate scores from prospective moves.

--- SuperMatch/Board/MatchResults.java
+++ SuperMatch/Board/MatchResults.java
@@ -2,11 +2,16 @@ package SuperMatch.Board;
 
 public class MatchResults
 {
+   public int x1, y1, x2, y2;
    public boolean hasFourMatch;
    public int[] typeCount;
    
-   public MatchResults()
+   public MatchResults(int ax, int ay, int bx, int by)
    {
+      x1 = ax;
+      y1 = ay;
+      x2 = bx;
+      y2 = by;
       hasFourMatch = false;
       typeCount = new int[TileType.values().length];
    }
@@ -16,5 +21,27 @@ public class MatchResults
       if(match.length >= 4)
          hasFourMatch = true;
       typeCount[match.type.ordinal()] += match.getValue() * combo;
+   }
+   
+   public int sumCounts()
+   {
+      int sum = 0;
+      for(int val : typeCount)
+         sum += val;
+      return sum;
+   }
+   
+   public MatchResults compare(MatchResults that)
+   {
+      // if one has a four-match and the other does not, return the one that does
+      if(this.hasFourMatch && !that.hasFourMatch)
+         return this;
+      if(that.hasFourMatch && !this.hasFourMatch)
+         return that;
+      // else return the one with a higher score
+      if(this.sumCounts() > that.sumCounts())
+         return this;
+      return that;
+      
    }
 }
--- SuperMatch/Board/TestGameBoard.java
+++ SuperMatch/Board/TestGameBoard.java
@@ -23,4 +23,20 @@ public class TestGameBoard extends GameBoard
       for(int y = 0; y < TILES_TALL; y++)
          tileArr[x][y] = new BoardTile(initialState.tileArr[x][y].getType());
    }
+   
+   public MatchResults getMoveResults(int x1, int y1, int x2, int y2)
+   {
+      MatchResults results = new MatchResults(x1, y1, x2, y2);
+      int combo = 1;
+      doSwap(x1, y1, x2, y2);
+      while(hasMatches())
+      {
+         Vector<MatchObj> matchList = getMatches();
+         for(MatchObj curMatch : matchList)
+            results.add(curMatch, combo);
+         removeMatches(false);
+         combo++;
+      }
+      return results;
+   }
 }
